5b619f1651bb2beed21778224038403a9801eecb
b1008240 <murakami_tsukasa@cyberagent.co.jp>
2017-08-28 17:12:18 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+$accessToken = 'ruBTtw8ReOZZXK7SN8j/Z3q4xlL8NwNmSpOUBwt+z+1nM/SCM7/t7DxIdxMn62EDfPHK0vOPxvcyFwpEeQ/LYVFUyA+q6Hl9Gh7/1gdR7NC8fDPTQCd14VlKusWX8jwwwvzwkP4GCY4IyphjskonAgdB04t89/1O/w1cDnyilFU=';
+//ユーザーからのメッセージ取得
+$json_string = file_get_contents('php://input');
+$jsonObj = json_decode($json_string);
+$type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
+//メッセージ取得
+$text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
+//ReplyToken取得
+$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
+//メッセージ以外のときは何も返さず終了
+if($type != "text"){
+	exit;
+}
+//返信データ作成
+$response_format_text = [
+	"type" => "text",
+	"text" => "金剛デース！"
+	];
+$post_data = [
+	"replyToken" => $replyToken,
+	"messages" => [$response_format_text]
+	];
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json; charser=UTF-8',
+    'Authorization: Bearer ' . $accessToken
+    ));
+$result = curl_exec($ch);
+curl_close($ch);
+?>
