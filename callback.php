1d98cab490d3ece9331a426275e22464a0e58392
murakami <murakami_tsukasa@cyberagent.co.jp>
2017-12-11 18:55:17 +0900

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,8 +1,8 @@
 <?php
 
-$accessToken = 'アクセストークン';
-
+$accessToken = 'トークン';
 $jsonString = file_get_contents('php://input');
+
 error_log($jsonString);
 $jsonObj = json_decode($jsonString);
 
@@ -10,76 +10,132 @@ $message = $jsonObj->{"events"}[0]->{"message"};
 $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
 $beacon = $jsonObj->{"events"}[0]->{"beacon"};
 
-if (strpos($message->{"text"},'sample') !== false || strpos($message->{"text"},'サンプル') !== false) {
+if (strpos($beacon->{"type"},'enter') !== false) {
+
   $messageData = [
       'type' => 'text',
-      'text' => 'サンプル'
+      'text' => '到着！'
   ];
 } elseif (strpos($message->{"text"},'貯金') !== false ) {
-require './vendor/autoload.php';
-Predis\Autoloader::register();
-$redis = new Predis\Client(getenv('REDIS_URL'));
+  require './vendor/autoload.php';
+  Predis\Autoloader::register();
+  $redis = new Predis\Client(getenv('REDIS_URL'));
 
-$m = 500;
-$data = $redis->get('o');
-$sum = $data + $m;
-$v = $redis->set('o', $sum);
-  $messageData = [
-      'type' => 'text',
-      'text' => '貯金'.$sum.'円！'
-  ];
+  //500円貯金用
+  $m = 500;
 
-} elseif (strpos($message->{"text"},'合計') !== false || strpos($message->{"text"},'ごうけい') !== false) {
-require './vendor/autoload.php';
-Predis\Autoloader::register();
-$redis = new Predis\Client(getenv('REDIS_URL'));
-
-$m = 500;
-$otu_data = $redis->get('o');
-$sum = $data + $otu_data;
-
-  $messageData = [
-      'type' => 'text',
-      'text' => '合計'.$sum.'円'
-  ];
-} elseif (strpos($message->{"text"},'教えて') !== false || strpos($message->{"text"},'おしえて') !== false) {
-require_once('phpQuery-onefile.php');
-$html = file_get_contents('http://www.will-be.co.jp/ikejiri_family.html?grid=rosen');
-$doc = phpQuery::newDocument($html);
-$titleArr = $doc[".blogbox_710"]->find("td");
-$arr = Array();
-foreach($titleArr as $val) {
- $arr += Array(pq($val)->text() => pq($val)->find("a")->attr("href"));
-}
-
-$today = date("n/j");
-$v = Array();
-foreach($arr as $key => $aaa) {
- if(strpos($key,$taday) !== false){
- $v += Array($aaa);
- }
-}
-
-$ans = Array();
-if (empty($v)){
-  $ans += Array("今日は新作物件はない");
-} else {
- $ans += '今日の新着物件 '.$v;
-}
-  $messageData = [
-      'type' => 'text',
-      'text' => $ans[0]
-  ];
-} else {
+  $data = $redis->get('otu');
+  $sum = $data + $m;
+  $v = $redis->set('otu', $sum);
     $messageData = [
         'type' => 'text',
-        'text' => $message->{"text"}
+        'text' => '貯金'.$sum.'円！'
     ];
+} elseif (strpos($message->{"text"},'教えて') !== false || strpos($message->{"text"},'おしえて') !== false) {
+
+  require_once('phpQuery-onefile.php');
+  $html = file_get_contents('http://www.will-be.co.jp/ikejiri_family.html?grid=rosen');
+  $doc = phpQuery::newDocument($html);
+  $titleArr = $doc[".blogbox_710"]->find("td");
+
+  $arr = Array();
+  foreach($titleArr as $val) {
+    $arr += Array(pq($val)->text() => pq($val)->find("a")->attr("href"));
+  }
+
+  $today = date("n/j");
+  $v = Array();
+  foreach($arr as $key => $aaa) {
+    if(strpos($key,$taday) !== false){
+      $v += Array($aaa);
+    }
+  }
+
+  $ans = Array();
+  if (empty($v)){
+    $ans += Array("新作物件なし");
+  } else {
+    $ans += '新着物件'.$v;
+  }
+  $messageData = [
+    'type' => 'text',
+    'text' => $ans[0]
+  ];
+} elseif ($message->{"text"} == 'ボタン') {
+  // ボタンタイプ
+  $messageData = [
+      'type' => 'template',
+      'altText' => 'ボタン',
+      'template' => [
+          'type' => 'buttons',
+          'title' => 'タイトルです',
+          'text' => '選択してね',
+          'actions' => [
+              [
+                  'type' => 'postback',
+                  'label' => 'webhookにpost送信',
+                  'data' => 'value'
+              ],
+              [
+                  'type' => 'uri',
+                  'label' => 'googleへ移動',
+                  'uri' => 'https://google.com'
+              ]
+          ]
+      ]
+  ];
+} elseif ($message->{"text"} == 'カルーセル') {
+  $messageData = [
+      'type' => 'template',
+      'altText' => 'カルーセル',
+      'template' => [
+          'type' => 'carousel',
+          'columns' => [
+              [
+                  'title' => 'カルーセル1',
+                  'text' => 'カルーセル1です',
+                  'actions' => [
+                      [
+                          'type' => 'postback',
+                          'label' => 'webhookにpost送信',
+                          'data' => 'value'
+                      ],
+                      [
+                          'type' => 'uri',
+                          'label' => 'google',
+                          'uri' => 'http://google.com'
+                      ]
+                  ]
+              ],
+              [
+                  'title' => 'カルーセル2',
+                  'text' => 'カルーセル2です',
+                  'actions' => [
+                      [
+                          'type' => 'postback',
+                          'label' => 'webhookにpost送信',
+                          'data' => 'value'
+                      ],
+                      [
+                          'type' => 'uri',
+                          'label' => 'テスト',
+                          'uri' => 'https://google.com/'
+                      ]
+                  ]
+              ],
+          ]
+      ]
+  ];
+} else {
+  $messageData = [
+    'type' => 'text',
+    'text' => $message->{"text"}
+  ];
 }
 
 $response = [
-    'replyToken' => $replyToken,
-    'messages' => [$messageData]
+  'replyToken' => $replyToken,
+  'messages' => [$messageData]
 ];
 error_log(json_encode($response));
 
